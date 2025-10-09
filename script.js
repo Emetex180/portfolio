9c0830e2e7e452bdf9ed6729f8c4a5ee853c563a
Emetex180 <emediongedoho1@gmail.com.com>
2025-10-09 20:45:30 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Optional animations
+// window.addEventListener('scroll', () => {
+//   const header = document.querySelector('header');
+//   if (window.scrollY > 50) {
+//     header.style.background = 'rgba(15,15,15,0.98)';
+//   } else {
+//     header.style.background = 'rgba(15,15,15,0.9)';
+//   }
+// });
